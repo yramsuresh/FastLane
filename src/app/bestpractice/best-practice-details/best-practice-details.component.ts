317c0ac983bb226e363a4a7dcf933411b94efbf7
Krutika Shekhar <Krutika.Shekhar@lntinfotech.com>
2020-05-20 11:18:27 +0530

    bestpractice details component

---
+++ src/app/bestpractice/best-practice-details/best-practice-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-best-practice-details',
+  templateUrl: './best-practice-details.component.html',
+  styleUrls: ['./best-practice-details.component.scss']
+})
+export class BestPracticeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
