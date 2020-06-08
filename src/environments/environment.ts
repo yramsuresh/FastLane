7537d1287c30893e178b4ee487f9be62a7a1fdcf
Vamsi Dasari <Vamsi.Dasari@lntinfotech.com>
2020-06-08 14:50:47 +0530

    added resuable side menu component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "../assets/Data/data.Json"
+  apiUrl: '../assets/Data/data.Json',
+  // apiUrl: 'http://localhost:5000/fastlane',
 };
 
 /*
