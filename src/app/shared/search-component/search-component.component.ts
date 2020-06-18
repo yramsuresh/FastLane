1729cbbf28ba9558f0494e2fd53dc9cef0b11ea1
gauthamrlti <57346682+gauthamrlti@users.noreply.github.com>
2020-06-18 15:40:40 +0530

    voice search integrated

--- src/app/shared/search-component/search-component.component.ts
+++ src/app/shared/search-component/search-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ChangeDetectorRef, OnChanges } from '@angular/core';
 // import { EventEmitter } from 'protractor';
 
 @Component({
@@ -6,13 +6,62 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
   templateUrl: './search-component.component.html',
   styleUrls: ['./search-component.component.scss'],
 })
-export class SearchComponentComponent implements OnInit {
+export class SearchComponentComponent implements OnInit, OnChanges {
   searchValue: string;
   @Output() search = new EventEmitter();
+  @Output() searchTextEmitter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() resetData: EventEmitter<string> = new EventEmitter<string>();
+  speechToText: string;
   constructor() {}
   keyup(event) {
     this.searchValue = event;
     this.search.emit(this.searchValue);
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //console.log('Inside speech-recognition resetUserData()');
+
+    if ('webkitSpeechRecognition' in window) {
+      // speech recognition API supported
+      console.log("supported")
+    } else {
+      console.log("not supported")
+      
+      // speech recognition API not supported
+    }
+  }
+  ngOnChanges(){
+    this.search.emit((<HTMLInputElement>document.getElementById('textToSearch')).value);
+  }
+  voiceSearch(): void {
+    //console.log('Inside speech-recognition voiceSearch()');
+    this.resetData.emit();
+    (<HTMLInputElement>document.getElementById('textToSearch')).value = '';
+    (<HTMLImageElement>document.getElementById('image')).src = 'assets/mic-animate.gif';
+    
+    const {webkitSpeechRecognition} = (window as any)
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-UK';
+    
+    recognition.onresult = (event) => {
+      this.speechToText = event.results[0][0].transcript;
+      (<HTMLInputElement>document.getElementById('textToSearch')).value = this.speechToText;
+      if(this.speechToText){
+        this.emitSearchValue();
+      }
+    }
+
+    recognition.start();
+
+    recognition.onspeechend = function() {
+      recognition.stop();
+    }
+
+    recognition.onaudioend = function() {
+      (<HTMLImageElement>document.getElementById('image')).src = 'assets/mic.gif';
+    }
+  }
+  emitSearchValue() : any {
+    //console.log('Inside speech-recognition emitSearchValue()');
+    this.searchTextEmitter.emit(this.speechToText);
+  }
 }
